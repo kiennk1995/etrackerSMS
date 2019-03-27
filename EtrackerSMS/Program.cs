961cc9365c9975ec2e2a69644981fe701717924e
KienNK <kiennk@runsystem.net>
2019-03-27 15:29:52 +0700

    aaa

---
+++ EtrackerSMS/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EtrackerSMS
+{
+    class Program
+    {
+        private string url = "https://www.etracker.cc/bulksms/send";
+        private string result = null;
+        static void Main(string[] args)
+        {
+            string user = ConfigurationManager.AppSettings["user"];
+            string pass = ConfigurationManager.AppSettings["pass"];
+
+        }
+
+        //public string ChoseMobile(string mobilephone,string phone)
+        //{
+
+        //}
+
+        private async void SendSMS(SendSMSModel smsModel)
+        {
+            try
+            {
+                //Validation of all the Parameter before submitting API request
+                string json = Json2KeyValue.JsonConvert.SerializeObject(smsModel);
+                using (HttpClient clinet = new HttpClient())
+                {
+                    StringContent content = new StringContent(json);
+                    content.Headers.ContentType.MediaType = "application/json";
+
+                    clinet.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                    HttpResponseMessage response = await clinet.PostAsync(new Uri(url), content);
+                    response.EnsureSuccessStatusCode();
+
+                    result = response.Content.ReadAsStringAsync().Result;
+                }
+            }
+            catch (Exception ex)
+            {
+                result = ex.Message;
+            }
+            finally
+            {
+
+            }
+        }
+    }
+}
---
+++ EtrackerSMS/SendSMSModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EtrackerSMS
+{
+    public class SendSMSModel
+    {
+        public string AccountID { get; set; }
+        public string MobilePhone { get; set; }
+        public string Phone { get; set; }
+        public string ContractName { get; set; }
+        public string Content { get; set; }
+        public string TitleMess { get; set; }
+    }
+}
